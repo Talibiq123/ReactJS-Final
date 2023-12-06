150e794fd7061033a36621bfa1b02937347967d4
Talibiq123 <talibsaeed90@gmail.com>
2023-12-06 19:41:33 +0530

    api in React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-// import EffectBasics from './Components/UseEffect/EffectBasics';
-import LiveSearching from './Components/UseEffect/LiveSearching';
 
 
 function App() {
 
     return (
         <div>
-            {/* <EffectBasics></EffectBasics> */}
-            <LiveSearching />
+        
         </div>
     )
 }
