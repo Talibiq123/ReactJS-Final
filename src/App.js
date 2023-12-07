cb8930d2e11fc8eb6d9bd9114e96fbba81ede68f
Talibiq123 <talibsaeed90@gmail.com>
2023-12-07 21:14:36 +0530

    APIs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Practice from './Components/Practice.js';
 
 
 function App() {
 
     return (
         <div>
-        
+            <Practice />
         </div>
     )
 }
---
+++ src/Components/Practice.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Practice() {
+    return (
+        <div>
+            <h1>Practice Component</h1>
+            <p>This paragraph is developed to practice deferent React components...</p>
+        </div>
+    )
+}
+
+export default Practice;
