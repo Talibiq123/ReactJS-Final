c62ff8e1ca3e8642ee80e591e61bfdde7b6d73ff
Talibiq123 <talibsaeed90@gmail.com>
2023-12-15 16:53:30 +0530

    context api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Auth from "./Components/Auth/Auth.js";
 import "./index.css";
 
 function App() {
 
     return (
         <div>
-            <Auth />
         </div>
     )
 }
